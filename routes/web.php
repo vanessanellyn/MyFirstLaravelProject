a21e65966756c8da18fc98ca52ff089c97381cd5
Vanessa Isidro <nellyn.isidro@gmail.com>
2023-11-10 16:59:44 +0800

    Model and Reading Data

--- routes/web.php
+++ routes/web.php
@@ -28,63 +28,22 @@ class Task
   }
 }
 
-$tasks = [
-  new Task(
-    1,
-    'Buy groceries',
-    'Task 1 description',
-    'Task 1 long description',
-    false,
-    '2023-03-01 12:00:00',
-    '2023-03-01 12:00:00'
-  ),
-  new Task(
-    2,
-    'Sell old stuff',
-    'Task 2 description',
-    null,
-    false,
-    '2023-03-02 12:00:00',
-    '2023-03-02 12:00:00'
-  ),
-  new Task(
-    3,
-    'Learn programming',
-    'Task 3 description',
-    'Task 3 long description',
-    true,
-    '2023-03-03 12:00:00',
-    '2023-03-03 12:00:00'
-  ),
-  new Task(
-    4,
-    'Take dogs for a walk',
-    'Task 4 description',
-    null,
-    false,
-    '2023-03-04 12:00:00',
-    '2023-03-04 12:00:00'
-  ),
-];
-
 Route::get('/', function () {
   return redirect()->route('tasks.index');
 });
 
-Route::get('/tasks', function () use ($tasks) {
+Route::get('/tasks', function () {
   return view('index', [
-      'tasks' => $tasks,
+    //'tasks' => \App\Models\Task::latest()->all() // gets all data
+    //'tasks' => \App\Models\Task::latest()->get() // get all data ordered by latest created
+    'tasks' => \App\Models\Task::latest()->where('completed', true)->get() 
   ]);
 })->name('tasks.index');
 
-Route::get('/tasks/{id}', function ($id) use ($tasks) {
-  $task = collect($tasks)->firstWhere('id', $id);
-
-  if(!$task) {
-    abort(Response::HTTP_NOT_FOUND);
-  }
-  
-  return view('show', ['task' => $task]);
+Route::get('/tasks/{id}', function ($id) {
+  return view('show', [
+    'task' => \App\Models\Task::findOrFail($id)
+  ]);
 })->name('tasks.show');
 
 Route::fallback(function () {
