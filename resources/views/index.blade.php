d4f1c31b93adf2e24415324586b271456d6f98e7
Vanessa Isidro <vnisidro@traxiontech.net>
2023-11-10 09:19:37 +0800

    Layouts Using Template Inheritance

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,13 @@
-<h1>
-  The List of Tasks
-</h1>
+@extends('layouts.app')
 
-<div>
-    @forelse ($tasks as $task)
-      <div>
-        <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>  
-      </div>
-    @empty
-      <p>There are no tasks!</p>
-    @endforelse
-</div>
+@section('title', 'The List of Tasks')
+
+@section('content')
+  @forelse ($tasks as $task)
+    <div>
+      <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>  
+    </div>
+  @empty
+    <p>There are no tasks!</p>
+  @endforelse
+@endsection
