10a52ba689a938a4c772a493ff032d7d006e0db3
Vanessa Isidro <nellyn.isidro@gmail.com>
2023-11-13 17:08:59 +0800

    Deleting Data

--- routes/web.php
+++ routes/web.php
@@ -61,6 +61,14 @@ Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
 })->name('tasks.update');
 
 
+Route::delete('/tasks/{task}', function (Task $task) {
+  $task->delete();
+
+  return redirect()->route('tasks.index')
+    ->with('success','Task deleted successfully.');
+})->name('tasks.destroy');
+
+
 Route::fallback(function () {
     return 'Still got somewhere!';    
 });
