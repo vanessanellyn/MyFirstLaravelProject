6e1ed98b13b36d5d1969ccd4de7c97e813e60e84
Vanessa Isidro <nellyn.isidro@gmail.com>
2023-11-13 17:00:30 +0800

    Reusability (Route Model Binding, Mass Assignment)

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'description', 'long_description'];
+    // $fillabe - where u put all the column names that you want Laravel to auto mass assignment
+    // protected $guarded = ['secret']; // opposite of fillable; fillable is more secure
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Requests\TaskRequest;
 use \App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,62 +20,46 @@ Route::get('/', function () {
   return redirect()->route('tasks.index');
 });
 
+
 Route::get('/tasks', function () {
   return view('index', [
     'tasks' => Task::latest()->get() 
   ]);
 })->name('tasks.index');
 
+
 Route::view('/tasks/create', 'create')->name('tasks.create');
 
-Route::get('/tasks/{id}/edit', function ($id) {
+
+Route::get('/tasks/{task}/edit', function (Task $task) {
   return view('edit', [
-    'task' => Task::findOrFail($id)
+    'task' => $task
   ]);
 })->name('tasks.edit');
 
-Route::get('/tasks/{id}', function ($id) {
+
+Route::get('/tasks/{task}', function (Task $task) {
   return view('show', [
-    'task' => Task::findOrFail($id)
+    'task' => $task
   ]);
 })->name('tasks.show');
 
-Route::post('/tasks', function (Request $request) {
-  $data = $request->validate([
-    'title' => 'required|max:255',
-    'description' => 'required',
-    'long_description' => 'required',
-  ]);
 
-  // Creates a new model
-  $task = new Task;
-  $task->title = $data['title'];
-  $task->description = $data['description'];
-  $task->long_description = $data['long_description'];
-
-  // Inserts or updates the model in the database
-  $task->save();
+Route::post('/tasks', function (TaskRequest $request) {
+  $task = Task::create($request->validated());
   
-  return redirect()->route('tasks.show', ['id' => $task->id])
+  return redirect()->route('tasks.show', ['task' => $task->id])
     ->with('success', 'Task created successfully!');
 })->name('tasks.store');
 
-Route::put('/tasks/{id}', function ($id, Request $request) {
-  $data = $request->validate([
-    'title' => 'required|max:255',
-    'description' => 'required',
-    'long_description' => 'required'
-  ]);
 
-  $task = Task::findOrFail($id);
-  $task->title = $data['title'];
-  $task->description = $data['description'];
-  $task->long_description = $data['long_description'];
-  $task->save();
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
+  $task->update($request->validated());
 
-  return redirect()->route('tasks.show', ['id' => $task->id])
+  return redirect()->route('tasks.show', ['task' => $task->id])
     ->with('success', 'Task updated successfully!');
 })->name('tasks.update');
+
 
 Route::fallback(function () {
     return 'Still got somewhere!';    
