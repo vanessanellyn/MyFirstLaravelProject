f353b357a71811c36cbab96bb4bac3b840013073
Vanessa Isidro <nellyn.isidro@gmail.com>
2023-11-13 15:07:28 +0800

    Edit Form

--- routes/web.php
+++ routes/web.php
@@ -21,11 +21,17 @@ Route::get('/', function () {
 
 Route::get('/tasks', function () {
   return view('index', [
-    'tasks' => Task::latest()->where('completed', true)->get() 
+    'tasks' => Task::latest()->get() 
   ]);
 })->name('tasks.index');
 
 Route::view('/tasks/create', 'create')->name('tasks.create');
+
+Route::get('/tasks/{id}/edit', function ($id) {
+  return view('edit', [
+    'task' => Task::findOrFail($id)
+  ]);
+})->name('tasks.edit');
 
 Route::get('/tasks/{id}', function ($id) {
   return view('show', [
@@ -52,6 +58,23 @@ Route::post('/tasks', function (Request $request) {
   return redirect()->route('tasks.show', ['id' => $task->id])
     ->with('success', 'Task created successfully!');
 })->name('tasks.store');
+
+Route::put('/tasks/{id}', function ($id, Request $request) {
+  $data = $request->validate([
+    'title' => 'required|max:255',
+    'description' => 'required',
+    'long_description' => 'required'
+  ]);
+
+  $task = Task::findOrFail($id);
+  $task->title = $data['title'];
+  $task->description = $data['description'];
+  $task->long_description = $data['long_description'];
+  $task->save();
+
+  return redirect()->route('tasks.show', ['id' => $task->id])
+    ->with('success', 'Task updated successfully!');
+})->name('tasks.update');
 
 Route::fallback(function () {
     return 'Still got somewhere!';    
