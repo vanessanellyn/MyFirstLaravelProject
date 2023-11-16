7c57a0b81c4549bafe479a28e6921db8c15a4d3a
Vanessa Isidro <nellyn.isidro@gmail.com>
2023-11-16 13:17:13 +0800

    Defining the One to Many Relationship

--- book-review/app/Models/Book.php
+++ book-review/app/Models/Book.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    public function reviews() // name of the relationship
+    {
+        // defines a one-to-many relationship
+        return $this->hasMany(Review::class);
+    }
 }
--- book-review/database/migrations/2023_11_16_004757_create_reviews_table.php
+++ book-review/database/migrations/2023_11_16_004757_create_reviews_table.php
@@ -18,6 +18,13 @@ return new class extends Migration
             $table->unsignedTinyInteger('rating');
 
             $table->timestamps();
+
+            // $table->unsignedBigInteger('book_id'); // foreign key
+            // $table ->foreign('book_id')->references('id')->on('books')
+            //     ->onDelete('cascade');
+
+            $table->foreignId('book_id')->constrained()
+                ->cascadeOnDelete();
         });
     }
 
